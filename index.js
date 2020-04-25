48c4a7202ef72c4dad9da9a23b571c98e8d26570
Jared S <jaredjames2020@gmail.com>
2020-04-25 01:00:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ class Bird{
     this.sex = sex;
     this.speak = function() {
     if (sex != male) return `${name} says squak!`;
-    };
-  }else return `It's me! ${name}, the parrot!`;
+    } else {
+      return `It's me! ${name}, the parrot!`}
+  };
 
 }
