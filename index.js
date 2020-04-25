d108192bcc5d0dad0f03d2f22f1b6b75ed6157c9
Jared S <jaredjames2020@gmail.com>
2020-04-25 00:46:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+class Cat{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+    this.speak = `${name} says meow!`
+  }
+  
+}
+
+class Dog {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+    this.speak = `${name} says woof!`
+  }
+}
+
+class Bird{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+    this.speak = `${name} says`
+  }
+}
